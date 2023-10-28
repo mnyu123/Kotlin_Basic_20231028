fd16bb5b1f55f3f3912a616f53b7bf04abb1e060
mnyu123 <whdudwo5582@gmail.com>
2023-10-28 16:20:55 +0900

    코틀린 변수 값 변경

--- app/src/main/java/com/example/kotlin_basic_20231028/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_basic_20231028/MainActivity.kt
@@ -20,7 +20,9 @@ class MainActivity : AppCompatActivity() {
         binding.logBtn.setOnClickListener { 
             // 로그 버튼 이벤트
             // 로그 버튼이 클릭 되면 실행되는 코드
-            Log.d("메인화면" , "로그 버튼 클릭됨.")
+            Log.d("메인화면" , "로그 버튼 클릭됨.") // d는 디버깅용 로그
+            Log.e("메인화면","e로 로그 찍어보기") // 에러 찾기용 로그
+            Log.wtf("메인화면","빡칠때 사용하는 로그") // 빡칠때 쓰는 로그
             Toast.makeText(this,"로그버튼 클릭함.",Toast.LENGTH_SHORT).show()
         }
 
@@ -28,6 +30,22 @@ class MainActivity : AppCompatActivity() {
             // 토스트 버튼 이벤트
             // 토스트 버튼이 클릭 되면 실행되는 코드
             Toast.makeText(this,"토스트 버튼 클릭함.",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.varBtn.setOnClickListener {
+            // 코틀린 변수 문법 연습 버튼 이벤트
+            // 코틀린 변수 연습 버튼이 클릭되면 실행되는 코드
+            // 내 이름을 저장할 변수(String) 타입 => 변동 가능성 있음.
+
+            var myName : String
+            // 내 이름을 저장할 변수 , String 형으로 생성
+
+            myName = "조영재"
+
+            // 이름 변경
+            myName = "영재맨"
+
+            Toast.makeText(this, myName, Toast.LENGTH_SHORT).show()
         }
     }
 }
